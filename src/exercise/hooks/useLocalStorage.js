1d3e2cb4f60a4f15fd82b33f225b1109df7c95c4
Ahmad-Dorri <ahmaddorri110@gmail.com>
2023-11-21 17:31:25 +0330

    making useLocalStorage flexable

--- src/exercise/hooks/useLocalStorage.js
+++ src/exercise/hooks/useLocalStorage.js
@@ -1,11 +1,27 @@
 import * as React from 'react'
 
-export const useLocalStorage = key => {
-  const [value, setValue] = React.useState(() =>
-    window.localStorage.getItem(key),
-  )
+export const useLocalStorage = (
+  key,
+  defaultValue = '',
+  {serialize = JSON.stringify, deserialize = JSON.parse} = {},
+) => {
+  const [value, setValue] = React.useState(() => {
+    const valueInLocalStorage = window.localStorage.getItem(key)
+    if (valueInLocalStorage) {
+      return deserialize(valueInLocalStorage)
+    }
+    return typeof defaultValue === 'function' ? defaultValue() : defaultValue
+  })
+
+  const prevKeyRef = React.useRef(key)
+
   React.useEffect(() => {
-    window.localStorage.setItem(key, value)
-  }, [value])
+    const prevKey = prevKeyRef.current
+    if (prevKey !== key) {
+      window.localStorage.removeItem(prevKey)
+    }
+    prevKeyRef.current = key
+    window.localStorage.setItem(key, serialize(value))
+  }, [value, key, serialize])
   return [value, setValue]
 }
